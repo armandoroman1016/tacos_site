7e9178a9809ec7b14a713ba7f912c4f57e61ff5e
Armando Roman <armandoroman1016@gmail.com>
2020-01-29 18:27:16 -0800

    nav bar opening on click

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+let hamburgerButton = document.querySelector('.mobile_menu_icon')
+let mobileLinks =  document.querySelector('.mobile-nav-links')
+
+hamburgerButton.addEventListener('click', (e) => {
+    
+    mobileLinks.classList.add('show')
+    
+
+
+})
