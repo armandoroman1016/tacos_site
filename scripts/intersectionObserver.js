6ee7d476360875b8f532add602a330fef55990ca
Armando Roman <armandoroman1016@gmail.com>
2020-01-30 13:49:06 -0800

    navbar background toggling on intersection

--- scripts/intersectionObserver.js
+++ scripts/intersectionObserver.js
@@ -1,20 +1,34 @@
 const nav = document.querySelector('nav')
+const banner = document.querySelector('.banner')
 
-const cb = (entries, observer) => {
+const bannerCb = (entries, observer) => {
 
     console.log(observer)
     entries.forEach(entry => {
-        console.log('inside cb:', entry.target)
+        if(!entry.isIntersecting){
+            nav.classList.add("scrolled")
+        }else if(entry.isIntersecting){
+
+            if(nav.classList.contains('scrolled')){
+
+                nav.classList.remove('scrolled')
+
+            }else{
+                return;
+
+            }
+
+        }
     })
 
 }
 
-const options = {
+const bannerOptions = {
     root: null, // is the viewport
     threshold: 0,
-    rootMargin: "-50px"
+    rootMargin: "-100px 0px"
 }
 
-const observer = new IntersectionObserver(cb, options);
+const bannerObserver = new IntersectionObserver(bannerCb, bannerOptions);
 
-observer.observe(nav)
+bannerObserver.observe(banner)
